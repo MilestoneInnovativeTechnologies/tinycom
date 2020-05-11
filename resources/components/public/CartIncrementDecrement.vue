01e541e6e7c4ac614cb88e19ed643fb05b077e1c
Firose <firumon@github.com>
2020-05-11 16:27:49 +0530

    Cart increment and decrement component created

---
+++ resources/components/public/CartIncrementDecrement.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="form-inline">
+        <button @click.prevent="decrement" class="btn btn-sm btn-info"><i class="fas text-white fa-level-up-alt fa-rotate-180"></i></button>
+        <input type='text' name='quantity' v-model="quantity" class='form-control form-control-sm border-0 text-center font-weight-bold text-info' style="width: 2rem" />
+        <button @click.prevent="increment" class="btn btn-sm btn-info" ><i class="fas text-white fa-level-up-alt"></i></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CartIncrementDecrement",
+        props: ['id'],
+        computed: {
+            quantity(){ return this.$store.getters['CART/itemQuantity'](this.id) },
+        },
+        methods: {
+            increment(){ this.$store.dispatch('CART/increment',this.id) },
+            decrement(){ this.$store.dispatch('CART/decrement',this.id) },
+        },
+    }
+</script>
