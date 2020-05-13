ab3ad6529d04f99e43e41c2cbd6ccda7664736ed
Firose <firumon@github.com>
2020-05-13 16:45:13 +0530

    Disk configured as it depends on sub domain

--- config/filesystems.disks.php
+++ config/filesystems.disks.php
@@ -1,11 +1,13 @@
 <?php
 
+$hosts = explode('.',request()->getHost()); array_pop($hosts); $domain = implode('.',$hosts); $domain = $domain ? ('/' . $domain) : '';
+
     return [
 
         'media' => [
             'driver' => 'local',
-            'root' => public_path('uploads/media'),
-            'url' => env('APP_URL').'/uploads/media',
+            'root' => public_path('uploads/media' . $domain),
+            'url' => env('APP_URL').'/uploads/media'.$domain,
             'visibility' => 'public',
         ]
 
