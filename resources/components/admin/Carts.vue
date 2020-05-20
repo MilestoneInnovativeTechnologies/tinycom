036cb0daefb3bb96d2642efd2b322fb8887cb318
Firose <firumon@github.com>
2020-05-20 15:37:20 +0530

    Carts limit changed to 10 and sorted descending order

--- resources/components/admin/Carts.vue
+++ resources/components/admin/Carts.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="carts-all">
-        <RecordList :records="carts" :layout="layout" truncate="20" title="Carts" route="cart" />
+        <RecordList :records="carts" :layout="layout" truncate="20" :limit="10" title="Carts" route="cart" />
     </section>
 </template>
 
@@ -11,7 +11,7 @@
             layout: { ID:'id',Customer:'customer_name',Status:'status','Updated':'updated',Source:'source_title' }
         } },
         computed: {
-            carts(){ return this.$store.getters["CARTS/all"] },
+            carts(){ return _(this.$store.getters["CARTS/all"]).sortBy(({ updated_at }) => new Date(updated_at).getTime()).reverse().value() },
         }
     }
 </script>
