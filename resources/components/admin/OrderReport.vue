7fdf9718820d64f94883a2d637dd9c1df9893e9b
Firose <firumon@github.com>
2020-05-28 13:51:10 +0530

    Graph plotting made corrected

--- resources/components/admin/OrderReport.vue
+++ resources/components/admin/OrderReport.vue
@@ -1,43 +1,46 @@
 <template>
-    <div class="card" style="max-width: 65vh">
-        <div class="card-body">
-            <graph-bar :padding-left="30" :padding-top="30" :padding-bottom="0" :padding-right="0" :height="graph_height" :width="graph_width" :axis-reverse="true" :labels="graph_days" :values="bar_totals" :axis-min="axis_min" :axis-max="axis_max" :axis-step="10" display="all">
-                <note text="Collection Report"></note>
-            </graph-bar>
+    <section class="order-reports">
+        <div class="card" style="max-width: 65vh">
+            <div class="card-body">
+                <graph-bar :key="'graph-bar-collection-'+graph_key" :padding-left="30" :padding-top="30" :padding-bottom="0" :padding-right="0" :height="graph_height" :axis-reverse="true" :labels="graph_days" :values="bar_totals" :axis-min="axis_min" :axis-max="axis_max" :axis-step="10" display="all">
+                    <note text="Collection Report"></note>
+                </graph-bar>
+            </div>
+            <div class="card-body">
+                <table class="table table-sm">
+                    <thead><tr><th>Day</th><th>Orders</th><th>Processed</th><th>Collection</th></tr></thead>
+                    <tbody>
+                    <tr v-for="st in statics" :key="'order-report-static-day-'+st.day">
+                        <td>{{ st.day }}</td>
+                        <td>{{ st.orders }}</td>
+                        <td>{{ st.delivered }}</td>
+                        <td>{{ st.total }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-body pt-0">
+                <graph-line :key="'graph-line-collection-'+graph_key" :padding-left="30" :padding-top="30" :padding-bottom="30" :padding-right="0" :height="graph2_height" shape="curve" :axis-full-mode="true" :labels="graph_days" :values="line_values" disply="all">
+                    <note text="Collection"></note>
+                </graph-line>
+            </div>
+            <div class="card-body pt-0">
+                <graph-line :key="'graph-line-orders-'+graph_key" :padding-left="0" :padding-top="30" :padding-bottom="50" :padding-right="0" :height="graph2_height" :axis-full-mode="true" shape="curve" :opacity="0.6" :borderline="true" :labels="graph_days" :values="area_values">
+                    <note text="Orders"></note>
+                    <legends :names="area_names"></legends>
+                    <guideline :tooltip-y="true"></guideline>
+                </graph-line>
+            </div>
         </div>
-        <div class="card-body">
-            <table class="table table-sm">
-                <thead><tr><th>Day</th><th>Orders</th><th>Processed</th><th>Collection</th></tr></thead>
-                <tbody>
-                <tr v-for="(st,d) in statics" :key="'order-report-static-day-'+st.day">
-                    <td>{{ st.day }}</td>
-                    <td>{{ st.orders }}</td>
-                    <td>{{ st.delivered }}</td>
-                    <td>{{ st.total }}</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="card-body pt-0">
-            <graph-line :padding-left="30" :padding-top="30" :padding-bottom="30" :padding-right="0" :width="graph_width" :height="graph2_height" shape="curve" :axis-full-mode="true" :labels="graph_days" :values="line_values" disply="all">
-                <note text="Collection"></note>
-            </graph-line>
-        </div>
-        <div class="card-body pt-0">
-            <graph-line :padding-left="0" :padding-top="30" :padding-bottom="50" :padding-right="0" :width="graph_width" :height="graph2_height" :axis-full-mode="true" shape="curve" :opacity="0.6" :borderline="true" :labels="graph_days" :values="area_values">
-                <note text="Orders"></note>
-                <legends :names="area_names"></legends>
-                <guideline :tooltip-y="true"></guideline>
-            </graph-line>
-        </div>
-    </div>
+    </section>
 </template>
 
 <script>
     export default {
         name: "OrderReport",
         data(){ return {
-            graph_width: window.outerWidth-100,
+            graph_key: 0,
+            graph_width: 0,
             graph_height: 160, graph2_height: 200,
             days: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
             avoid_status: ['New','Cancelled']
@@ -58,6 +61,7 @@
         },
         methods: {
             total(array){ return _.sumBy(array,({ amount }) => _.toNumber(amount)) },
-        }
+        },
+        beforeRouteEnter(to,from,next){ next($vm0 => setTimeout(($vm0) => $vm0.graph_key++,1000,$vm0)); },
     }
 </script>
