22a1dff2e1a980c53f1c0e7f02317103d9269283
Firose Hussain <github@firumon.com>
2021-12-11 19:55:51 +0530

    Development compatibility

--- resources/quasar/src-admin/assets/formData.js
+++
@@ -1,7 +0,0 @@
-export default {
-  obj(obj){
-    let frmData = new FormData();
-    if(obj) for(let x in obj) frmData.append(x,obj[x]);
-    return frmData;
-  }
-}
--- resources/quasar/src-public/boot/init.js
+++
@@ -1,6 +0,0 @@
-global._ = require('lodash')
-import filters from './../filters/common'
-
-export default async ({ Vue }) => {
-  _.forEach(filters,(fn,fl) => Vue.filter(fl,fn))
-}
